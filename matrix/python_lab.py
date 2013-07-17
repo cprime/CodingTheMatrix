7dbf38cf8653239284ee1c040fb48ae1534a1c67
Colden Prime <colden@intrepid.io>
2013-07-17 07:54:38 -0400

    complete lab's questions 1-10

--- matrix/python_lab.py
+++ matrix/python_lab.py
@@ -1,40 +1,40 @@
 ## Task 1
-minutes_in_week = ...
+minutes_in_week = 60 * 24 * 7
 
 ## Task 2
-remainder_without_mod = ...
+remainder_without_mod = 2304811 - ((2304811 // 47) * 47)
 
 ## Task 3
-divisible_by_3 = ...
+divisible_by_3 = (673 + 909) % 3 == 0
 
 ## Task 4
 x = -9
 y = 1/2
-statement_val = ...
+statement_val = 1.0
 
 ## Task 5
-first_five_squares = { ... for _ in {1,2,3,4,5} }
+first_five_squares = { x**2 for x in {1,2,3,4,5} }
 
 ## Task 6
-first_five_pows_two = { ... for _ in {0,1,2,3,4} }
+first_five_pows_two = { 2**x for x in {0,1,2,3,4} }
 
 ## Task 7: enter in the two new sets
-X1 = { ..., ..., ... }
-Y1 = { ..., ..., ... }
+X1 = { 5, 2, 3 }
+Y1 = { 2, 11, 7 }
 
 ## Task 8: enter in the two new sets
-X2 = { ..., ..., ... }
-Y2 = { ..., ..., ... }
+X2 = { 2, 4, 8 }
+Y2 = { 3, 6, 12 }
 
 ## Task 9
 base = 10
 digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-three_digits_set = { ... }
+three_digits_set = {(x * base**2) + (y * base**1) + (z * base**0) for x in digits for y in digits for z in digits}
 
 ## Task 10
 S = {1, 2, 3, 4}
 T = {3, 4, 5, 6}
-S_intersect_T = { ... }
+S_intersect_T = {x for x in S if x in T}
 
 ## Task 11
 L_average = ... # average of: [20, 10, 15, 75]
