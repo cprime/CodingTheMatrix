d8ee06fdc4e31e340be3a817a39c183414f93fcf
Colden Prime <coldenprime@gmail.com>
2013-08-19 09:22:26 -0400

    finish hw4

--- matrix/hw4.py
+++ matrix/hw4.py
@@ -4,6 +4,7 @@
 from GF2 import one
 from math import sqrt, pi
 from matutil import coldict2mat
+from matutil import rowdict2mat
 from solver import solve
 from vec import Vec
 
@@ -15,9 +16,9 @@ from vec import Vec
 #
 # For example, [1, 3, 5] would mean 1*[2,0,4,0] + 3*[0,1,0,1] + 5*[0,0,-1,-1]
 
-rep_1 = [...]
-rep_2 = [...]
-rep_3 = [...]
+rep_1 = [1,1,0]
+rep_2 = [.5,1,1]
+rep_3 = [0,1,-1]
 
 
 
@@ -25,10 +26,10 @@ rep_3 = [...]
 # For each part, please provide your solution as a list of the coefficients for
 # the generators of V.
 
-lin_comb_coefficients_1 = [...]
-lin_comb_coefficients_2 = [...]
-lin_comb_coefficients_3 = [...]
-lin_comb_coefficients_4 = [...]
+lin_comb_coefficients_1 = [3,-1,1]
+lin_comb_coefficients_2 = [.5,-1.5,1]
+lin_comb_coefficients_3 = [.5,-5.5,4]
+lin_comb_coefficients_4 = [1,-2,1]
 
 
 
@@ -37,9 +38,9 @@ lin_comb_coefficients_4 = [...]
 # For each part, please provide your solution as a list of the coefficients for
 # the generators of V.
 
-gf2_rep_1 = [...]
-gf2_rep_2 = [...]
-gf2_rep_3 = [...]
+gf2_rep_1 = [one,0,one,0]
+gf2_rep_2 = [one,0,0,one]
+gf2_rep_3 = [one,one,0,one]
 
 
 
@@ -48,10 +49,10 @@ gf2_rep_3 = [...]
 # For each part, please provide your solution as a list of the coefficients for
 # the generators of V.
 
-gf2_lc_rep_1 = [...]
-gf2_lc_rep_2 = [...]
-gf2_lc_rep_3 = [...]
-gf2_lc_rep_4 = [...]
+gf2_lc_rep_1 = [0,0,0,0,one,one,0,0]
+gf2_lc_rep_2 = [0,0,0,0,0,0,one,one]
+gf2_lc_rep_3 = [one,0,0,one,0,0,0,0]
+gf2_lc_rep_4 = [0,0,0,one,0,one,0,0]
 
 
 
@@ -59,18 +60,18 @@ gf2_lc_rep_4 = [...]
 # For each part, please provide your solution as a list of the coefficients for
 # the generators of V.
 
-lin_dep_R_1 = [...]
-lin_dep_R_2 = [...]
-lin_dep_R_3 = [...]
+lin_dep_R_1 = [2,-1,-1]
+lin_dep_R_2 = [4,-1,4/7]
+lin_dep_R_3 = [-.1,0,0,1/3,1]
 
 
 
 ## Problem 6
 # Please record your solution as a list of coefficients
 
-linear_dep_R_1 = [...]
-linear_dep_R_2 = [...]
-linear_dep_R_3 = [...]
+linear_dep_R_1 = [1,-1,3]
+linear_dep_R_2 = [2,1/pi,1/sqrt(2)]
+linear_dep_R_3 = [1,1,1,1,1]
 
 
 
@@ -79,29 +80,29 @@ linear_dep_R_3 = [...]
 # Assign sum_to to the vector that you are expressing as a linear combination
 # of the other two.  Write the name of the vector as a STRING.  i.e. 'u' or 'w'
 
-u = ...
-v = ...
-w = ...
-sum_to = ...
+u = -1
+v = 1
+w = 0
+sum_to = 'w'
 
 
 
 ## Problem 8
 # Please use the Vec class to represent your vectors
 
-indep_vec_1 = Vec({...}, {...})
-indep_vec_2 = Vec({...}, {...})
-indep_vec_3 = Vec({...}, {...})
-indep_vec_4 = Vec({...}, {...})
+indep_vec_1 = Vec({1,2,3}, {1:1,2:0,3:0})
+indep_vec_2 = Vec({1,2,3}, {1:0,2:1,3:0})
+indep_vec_3 = Vec({1,2,3}, {1:0,2:0,3:1})
+indep_vec_4 = Vec({1,2,3}, {1:1,2:1,3:1})
 
 
 
 ## Problem 9
 # Please give your solution as a list of coefficients of the linear combination
 
-zero_comb_1 = [...]
-zero_comb_2 = [...]
-zero_comb_3 = [...]
+zero_comb_1 = [one,one,0,one]
+zero_comb_2 = [0,one,one,one]
+zero_comb_3 = [one,one,0,0,one]
 
 
 
@@ -109,27 +110,27 @@ zero_comb_3 = [...]
 # Please give your solution as a list of coefficients of the vectors
 # in the set in order (list the coefficient for v_i before v_j if i < j).
 
-sum_to_zero_1 = [...]
-sum_to_zero_2 = [...]
-sum_to_zero_3 = [...]
-sum_to_zero_4 = [...]
+sum_to_zero_1 = [0,one,0,one,one]
+sum_to_zero_2 = [0,one,0,one,one,0,0]
+sum_to_zero_3 = [one,0,one,one,one]
+sum_to_zero_4 = [one,one,one,one,one,0,0]
 
 
 
 ## Problem 11
 ## Please express your answer a list of ints, such as [1,0,0,0,0]
 
-exchange_1 = [...]
-exchange_2 = [...]
-exchange_3 = [...]
+exchange_1 = [0,0,1,0,0]
+exchange_2 = [0,0,0,1,0]
+exchange_3 = [0,0,0,0,1]
 
 
 ## Problem 12
 # Please give the name of the vector you want to replace as a string (e.g. 'v1')
 
-replace_1 = ...
-replace_2 = ...
-replace_3 = ...
+replace_1 = 'v3'
+replace_2 = 'v1'
+replace_3 = 'v1'
 
 
 
@@ -148,7 +149,7 @@ def rep2vec(u, veclist):
         >>> rep2vec(Vec({0,1,2}, {0:2, 1:4, 2:6}), [a0,a1,a2]) == Vec({'a', 'c', 'b', 'd'},{'a': 2, 'c': 6, 'b': 4, 'd': 0})
         True
     '''
-    pass
+    return u * rowdict2mat(veclist)
 
 
 
@@ -168,7 +169,7 @@ def vec2rep(veclist, v):
         >>> vec2rep([a0,a1,a2], Vec({'a','b','c','d'}, {'a':3, 'c':-2})) == Vec({0, 1, 2},{0: 3.0, 1: 0.0, 2: -2.0})
         True
     '''
-    pass
+    return solve(coldict2mat(veclist),v)
 
 
 
@@ -197,7 +198,18 @@ def is_superfluous(L, i):
         >>> is_superfluous([a0,a1,a2,a3], 1)
         False
     '''
-    pass
+    L = L[:]
+    if len(L) == 1:
+        return False
+    b = L[i]
+    del L[i]
+    M = coldict2mat(L)
+    x = solve(M,b)
+    res = b - (M * x)
+    if res * res < 10 ** -14:
+        return True
+    else:
+        return False
 
 
 
@@ -224,7 +236,10 @@ def is_independent(L):
     >>> is_independent(vlist[5:])
     True
     '''
-    pass
+    for i in range(len(L)):
+        if is_superfluous(L,i):
+            return False
+    return True
 
 
 
@@ -246,7 +261,14 @@ def superset_basis(S, L):
         >>> superset_basis([a0, a3], [a0, a1, a2]) == [Vec({'a', 'c', 'b', 'd'},{'a': 1}), Vec({'a', 'c', 'b', 'd'},{'b':1}),Vec({'a', 'c', 'b', 'd'},{'c': 1})]
         True
     '''
-    pass
+    T = S[:]
+    for v in L:
+        if v in T:
+            continue
+        T.append(v)
+        if not is_independent(T):
+            T.pop()
+    return T
 
 
 
@@ -265,5 +287,13 @@ def exchange(S, A, z):
         >>> exchange(S, A, z) == Vec({0, 1, 2, 3},{0: 0, 1: 0, 2: 1, 3: 0})
         True
     '''
-    pass
+    L = [v for v in S if not(v in A)]
+    for w in L:
+        veclist = [v for v in L if w != v]
+        M = coldict2mat(veclist + A + [z])
+        x = solve(M,w)
+        res = w - (M * x)
+        if res * res < 10 ** -14:
+            return w
+    return None
 
